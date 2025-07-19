adbc1b777ec93cef1bb8e91894aa512e5cb4b2e6
michael trifonov <misha.a.t@gmail.com>
2025-07-18 21:05:49 -0400

    Add base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://michaeltrifonov.github.io',
+  base: '/gabriellesophia.github.io',
   integrations: [react(), mdx()],
   build: {
     // Inline small CSS to reduce requests
